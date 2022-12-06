4a90bf2fb25ab20aa5eada20eb113191d7e5bd5d
Andrew Berg <andrew_berg@discovery.com>
2022-12-05 16:58:24 -0800

    start new parser

--- perl/joy_parser.pm
+++ perl/joy_parser.pm
@@ -2,6 +2,8 @@ package joy_parser;
 
 use v5.24.1;
 use lib ".";
+use Marpa::R2;
+
 use joy_types;
 
 sub new {
@@ -28,17 +30,32 @@ sub _init {
   $self->{error} = undef;
 }
 
+my $dsl = <<'END_OF_DSL';
+:default ::= action => [name,values]
+lexeme default = latm => 1
+
+Script ::= Statement *
+Statement ::= 
+  Definition
+  | Function action => ::do_function
+Definition ::= Symbol '=' Function action => ::do_definition
+Function ::= Atom | Sequence
+Sequence ::= 
+  '[' Atoms ']' 
+  | '[' ']'
+Atoms ::= Atoms Atom | Atom | 
+Atom ::= Number | Symbol | String
+
+:discard ~ whitespace
+whitespace ~ [\s]+
+
+END_OF_DSL
+
 sub parse {
   my $self = shift;
   my $text = shift;
   my $symbols = shift;
 
-  print("parse()\n");
-  $self->{input} = $text;
-  $self->{used} = 0;
-  $self->{symbols} = $symbols;
-  $self->_script();
-  $self->{result} = $self->{sequences}[0];
   return not $self->{error};
 }
 
@@ -54,160 +71,5 @@ sub error {
   return $self->{error};
 }
 
-sub _script {
-  my $self = shift;
-  print "_script\n";
-
-  while ($self->_whitespace() or 
-         $self->_sequence() or 
-         $self->_atom() or 
-         $self->_comment()) {
-    print "_script looping\n";
-  }
-}
-
-sub _sequence {
-  my $self = shift;
-  print "_sequence\n";
-
-  my $used = $self->{used};
-  return 0 unless $self->_consume('[');
-  unshift @{$self->{sequences}}, [];
-  $self->_script();
-  my $val = shift @{$self->{sequences}};
-  unless ($self->_consume(']')) {
-    $self->{used} = $used;
-    return 0;
-  }
-  $self->_append(joy_types::sequence->new($val));
-  return 1;
-}
-
-sub _atom {
-  # takes an atom from the input, and adds it to the current sequence.
-  my $self = shift;
-  print "_atom\n";
-
-  return 1 if $self->_symbol();
-  return 1 if $self->_number();
-  return 1 if $self->_string();
-  return 0;
-}
-
-sub _number {
-  # looks for a number and adds it to the current sequence.
-  my $self = shift;
-  print "_number\n";
-
-  if (my $val = $self->_match(qr(^\d+(\.\d+)?))) {
-    $self->_append(joy_types::number->new($val));
-    return 1;
-  }
-  return 0;
-}
-
-sub _symbol {
-  # looks for a symbol and adds it to the current sequence.
-  my $self = shift;
-
-  if (my $val = $self->_match(qr(^[^\d\s][^\s]+))) {
-    $self->_append(joy_types::symbol->new($val, $self->{symtab}{$val}));
-    print "_symbol:1\n";
-    return 1;
-  }
-  print "_symbol:0\n";
-  return 0;
-}
-
-sub _string {
-  # looks for a string and adds it to the current sequence.
-  my $self = shift;
-  print "_string\n";
-
-  if (my $val = $self->_match(qr(^"[^"]*"))) {
-    $self->_append(joy_types::string->new($val));
-    return 1;
-  }
-  return 0;
-}
-
-sub _whitespace {
-  # looks for one or more whitespace characters and discards them.
-  my $self = shift;
-  print "_whitespace\n";
-
-  return length($self->_match(qr(^\s+)));
-}
-
-sub _comment {
-  # looks for a comment and discards it.
-  my $self = shift;
-  print "_comment\n";
-
-  return $self->_match(qr(^--.*\n));
-}
-
-sub _append {
-  # takes a value of some sort and adds it to the current constructing
-  # seuqence.
-  my $self = shift;
-  my $val = shift;
-
-  push @{$self->{sequences}}, $val;
-}
-
-sub _matchahead {
-  # takes a regular expression, and if it matches against the current
-  # point of the input returns true.
-  my $self = shift;
-  my $re = shift;
-
-  my $rest = substr($self->{input}, $self->{used});
-  return scalar($rest =~ $re);
-}
-
-sub _match {
-  # takes a regular expression, and if it matches against the current
-  # point of the input it removes the match from the input and returns
-  # it.  Returns false (empty string: '') otherwise.
-  my $self = shift;
-  my $re = shift;
-
-  my $rest = substr($self->{input}, $self->{used});
-  return '' unless $rest =~ $re;
-  my $result = substr($rest, $-[0], $+[0] - $-[0]);
-  printf "match success, '%s' ('%s'+%d) +[%d] -[%d]\n", $result, $rest, $self->{used}, $+[0], $-[0];
-  $self->{used} += $_[0] - $-[0];
-  return $result;
-}
-
-sub _lookahead {
-  # takes a string, returns true if it matches the current point.
-  my $self = shift;
-  my $str = shift;
-  my $len = length($str);
-  return $str eq substr($self->{input}, $self->{used}, $len);
-}
-
-sub _consume { 
-  #  takes a string, and if it matches the current point of the input
-  #  it removes it from the input.  Returns true if matched, false
-  #  otherwise.
-  my $self = shift;
-  my $str = shift;
-  my $len = length($str);
-  
-  my $init = substr($self->{input}, $self->{used}, $len);
-  return 0 if $init ne $str;
-  $self->{used} -= $len;
-  return 1;
-}
-
-sub __shallowCopy {
-  my $h = shift;
-  my $result = {};
-  for (keys %$h) { $result->{$_} = $h->{$_} };
-  return $result;
-}
 
 1;
